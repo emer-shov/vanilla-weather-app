17f6de08748709f5ba58a1b50ba50d373ab5b741
emer-shov <emershovlin@gmail.com>
2023-05-24 18:21:03 +0100

    Update time

--- Vanilla-Weather-App/src/app.js
+++ Vanilla-Weather-App/src/app.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes <10){
+    minutes=`0${minutes}`;
+  }
+let days=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+  let day = days[date.getDay()];
+  return `${day} ${hours} ${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data.condition.description);
   console.log(response.data.temperature.current);
@@ -7,12 +31,13 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-
+  let dateElement = document.querySelector("date");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.time);
 }
 
 let apiKey = "2f431f06a6ad2abab0d3o47daa186t7f";
