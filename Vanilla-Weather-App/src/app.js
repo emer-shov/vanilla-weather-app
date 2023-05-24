12f74df6426e84ca59a488da8f7da38b8fa59737
emer-shov <emershovlin@gmail.com>
2023-05-24 23:38:47 +0100

    JS updates
    Struggling to get JS working- further updates needed

--- Vanilla-Weather-App/src/app.js
+++ Vanilla-Weather-App/src/app.js
@@ -5,9 +5,10 @@ function formatDate(timestamp) {
     hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
-  if (minutes <10){
+  if (minutes < 10){
     minutes=`0${minutes}`;
   }
+
 let days=[
     "Sunday",
     "Monday",
@@ -19,10 +20,31 @@ let days=[
 ];
 
   let day = days[date.getDay()];
-  return `${day} ${hours} ${minutes}`;
+  return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp){
+    let date= new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days =["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+    return days[day];
+}
+
+function displayForecast(response){
+
+}
+
+
+
+
+
+
+
+
+
 function displayTemperature(response) {
+    
   console.log(response.data.condition.description);
   console.log(response.data.temperature.current);
 
@@ -32,15 +54,24 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("date");
+  let iconElement=document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.time);
-}
+  dateElement.innerHTML = formatDate(response.data.time *1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily.condition.icon_url}few-clouds-day.png`
+     );
+iconElement.setAttribute("alt", response.data.condition.description);
+  }
+
 
 let apiKey = "2f431f06a6ad2abab0d3o47daa186t7f";
+let city="Lisbon";
 let apiUrl =
   "https://api.shecodes.io/weather/v1/current?lat=38.71667&lon=-9.13333&key=2f431f06a6ad2abab0d3o47daa186t7f&units=metric";
 
