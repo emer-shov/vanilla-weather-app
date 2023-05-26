6ca3827d08a92197146d59880dab49173997f49d
emer-shov <emershovlin@gmail.com>
2023-05-26 16:43:16 +0100

    Updated HTML and JavaScript for Homework week 7

--- Vanilla-Weather-App/src/app.js
+++ Vanilla-Weather-App/src/app.js
@@ -5,39 +5,35 @@ function formatDate(timestamp) {
     hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
-  if (minutes < 10){
-    minutes=`0${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
 
-let days=[
+  let days = [
     "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
-];
+    "Saturday",
+  ];
 
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
-function formatDay(timestamp){
-    let date= new Date(timestamp * 1000);
-    let day = date.getDay();
-    let days =["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    return days[day];
+  return days[day];
 }
 
-function displayForecast(response){
-
-}
-
+function displayForecast(response) {}
 
 function displayTemperature(response) {
-    
   console.log(response.data.condition.description);
   console.log(response.data.temperature.current);
 
@@ -47,65 +43,58 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
-  let iconElement=document.querySelector("#icon");
+  let iconElement = document.querySelector("#icon");
 
-  celciusTemperature= response.data.temperature.current;
+  celciusTemperature = response.data.temperature.current;
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.time *1000);
-  iconElement.setAttribute(
-    "src",
-    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon_url}few-clouds-day.png`
-     );
-iconElement.setAttribute("src", response.data.condition.icon_url);
-  }
-
-
-function search(city){
-    let apiKey = "2f431f06a6ad2abab0d3o47daa186t7f";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?lat=38.71667&lon=-9.13333&key=2f431f06a6ad2abab0d3o47daa186t7f&units=metric";
-axios.get(apiUrl).then(displayTemperature);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute("src", response.data.condition.icon_url);
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-
-  function handleSubmit(event){
-    event.preventDefault();
-    let cityInputElement= document.querySelector("#city-input");
-    search(cityInputElement.value);
-  }
-
-function displayFahrenheitTemperature(event){
-    event.preventDefault();
-    let fahrenheitTemperature=(celciusTemperature * 9/5) + 32;
-    celciusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    let temperatureElement=document.querySelector("#temperature");
-    temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
+function search(city) {
+  let apiKey = "2f431f06a6ad2abab0d3o47daa186t7f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=2f431f06a6ad2abab0d3o47daa186t7f&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-function displayCelciusTemperature (event){
-    event.preventDefault();
-    celciusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    let temperatureElement=document.querySelector("#temperature");
-    temperatureElement.innerHTML=Math.round(celciusTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
 
-let celciusTemperature=null;
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
+  celciusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
 
-let form=document.querySelector("#search-form");
+function displayCelciusTemperature(event) {
+  event.preventDefault();
+  celciusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
+}
+
+let celciusTemperature = null;
+
+let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink= document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click",displayFahrenheitTemperature);
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
-let celciusLink= document.querySelector("#celcius-link");
-celciusLink.addEventListener("click",displayCelciusTemperature);
-
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click", displayCelciusTemperature);
 
 search("New York");
